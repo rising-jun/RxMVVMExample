ea9a51b897ac4a3035383658faa981662abad6eb
kimdongjune <jun36817@naver.com>
2021-03-05 14:15:04 +0900

    added view

---
+++ CounterApp/CounterView.swift
@@ -0,0 +1,33 @@
+//
+//  CounterView.swift
+//  CounterApp
+//
+//  Created by 김동준 on 2021/03/05.
+//
+
+import Foundation
+import UIKit
+
+class CounterView: UIView{
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    func setup(){
+        backgroundColor = .white
+    }
+}
+
+extension UIView{
+    func addSubViews(_ views: UIView...) {
+        for view in views {
+            addSubview(view)
+        }
+    }
+}
--- CounterApp/ViewController.swift
+++ CounterApp/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var counterView = CounterView(frame: view.frame)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view = counterView
     }
 
 
