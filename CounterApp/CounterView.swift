ebc51a81e62117c80a40c70c47490909333b8f7a
kimdongjune <jun36817@naver.com>
2021-03-05 14:56:20 +0900

    complete UI

--- CounterApp/CounterView.swift
+++ CounterApp/CounterView.swift
@@ -29,6 +29,33 @@ class CounterView: UIView{
         backgroundColor = .white
         addSubViews(plusBtn, minusBtn, countLabel)
         
+        countLabel.text = "0"
+        countLabel.textColor = .black
+        countLabel.textAlignment = .center
+        countLabel.font = countLabel.font.withSize(24)
+        countLabel.snp.makeConstraints { (make) in
+            make.center.equalTo(self)
+            make.width.equalTo(100)
+            make.height.equalTo(80)
+        }
+        
+        plusBtn.setTitle("+", for: .normal)
+        plusBtn.setTitleColor(.black, for: .normal)
+        plusBtn.snp.makeConstraints { (make) in
+            make.width.equalTo(30)
+            make.height.equalTo(30)
+            make.leading.equalTo(countLabel.snp.trailing).offset(30)
+            make.centerY.equalTo(countLabel.snp.centerY)
+        }
+        
+        minusBtn.setTitle("-", for: .normal)
+        minusBtn.setTitleColor(.black, for: .normal)
+        minusBtn.snp.makeConstraints { (make) in
+            make.width.equalTo(30)
+            make.height.equalTo(30)
+            make.trailing.equalTo(countLabel.snp.leading).offset(-30)
+            make.centerY.equalTo(countLabel.snp.centerY)
+        }
         
     }
 }
